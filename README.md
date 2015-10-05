c7554d293e43ea28dd1eb78d84a2017569ee0b5d
Jonathan Hooper <jonathan@newaperio.com>
2015-10-05 00:48:29 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Intro
+
+PNGlitchGIF is a tool based off of [PNGlitch](https://github.com/ucnv/pnglitch) by [ucnv](https://github.com/ucnv/).
+
+PNGlitchGIF works by applying PNGlitch to an image multiple times with increasing then decreasing intensity.
+The results are stored in the directory named `frames`.
+The frames can then be used to created an animated GIF like the one seen below:
+
+![Example](https://github.com/jmhooper/PNGlitchGIF/blob/master/example.gif)
+
+# Usage
+
+1. Clone this repo
+2. Change the file named `input.png` to one of your choosing.
+3. Run `ruby glitch.rb`
+4. Find the frames the `frames` directory
---
+++ glitch.rb
@@ -0,0 +1,39 @@
+require_relative "PNGlitch/pnglitch"
+
+LETTERS_BASE = ((65...91).to_a + (97...123).to_a).map { |c| c.chr }
+
+def glitch(input, output, regex)
+	PNGlitch.open input do |png|
+	  png.each_scanline do |scanline|
+	    scanline.change_filter 1
+	  end
+	  png.glitch do |data|
+	  	data.gsub(regex, 'x')
+	  end
+	  png.save(output)
+	end
+end
+
+original_letters = LETTERS_BASE.dup
+migrated_letters = ['@']
+
+LETTERS_BASE.size.times do |i|
+	sample = original_letters.sample
+	original_letters.delete(sample)
+	migrated_letters += [sample]
+
+	puts migrated_letters.join
+
+	filename = "%03d" % i
+	glitch("input.png", "frames/#{filename}.png", /[#{migrated_letters.join}]/)
+end
+
+LETTERS_BASE.size.times do |i|
+	sample = migrated_letters.sample
+	migrated_letters.delete(sample)
+
+	puts migrated_letters.join
+
+	filename = "%03d" % (i + 52)
+	glitch("input.png", "frames/#{filename}.png", /[#{migrated_letters.join}]/)
+end
